96521d9657db8f6dd2a81cd5bda50bf55ab7d0cc
poundy <brett_pound@hotmail.com>
2026-03-12 14:13:30 +1100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+// Placeholder for enhancements
+// Example: set current year in footer
+(function(){
+  var el = document.getElementById('year');
+  if (el) el.textContent = new Date().getFullYear();
+})();
